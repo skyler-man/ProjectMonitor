471c83680e94575daea76290a8b19838e05018fb
liuyiqiang <liuyq@qdcares.cn>
2019-07-25 17:03:30 +0800

    修改bug

--- src/main/java/com/taocares/monitor/controller/ProjectRelationController.java
+++ src/main/java/com/taocares/monitor/controller/ProjectRelationController.java
@@ -1,14 +1,15 @@
 package com.taocares.monitor.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.taocares.monitor.dto.ProjectRelationDto;
-import com.taocares.monitor.dto.SonarProjectNameDto;
 import com.taocares.monitor.service.IProjectRelationService;
-import com.taocares.monitor.service.ISonarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,8 +25,9 @@ public class ProjectRelationController {
     private IProjectRelationService projectRelationService;
 
     @RequestMapping(value = "/set", method = RequestMethod.POST)
-    public void editProjectRelation(List<ProjectRelationDto> projectRelationDtos){
-        projectRelationService.editProjectRelation(projectRelationDtos);
+    public void editProjectRelation(@RequestParam(value = "content") String json){
+        List<ProjectRelationDto> relationSetJsonDtos = JSON.parseObject(json,ArrayList.class);
+        projectRelationService.editProjectRelation(relationSetJsonDtos);
     }
 
 }
